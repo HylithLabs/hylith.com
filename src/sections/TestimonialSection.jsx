52728c33e4cccc6c67a9f1bac8f5eea31ff65834
Raiyan Bin Rashid <raiyanbinrashid0@gmail.com>
2026-08-12 09:40:47 +0600

    feat: update testimonial section header text

--- src/sections/TestimonialSection.jsx
+++ src/sections/TestimonialSection.jsx
@@ -68,8 +68,8 @@ const TestimonialSection = () => {
   return (
     <section className="testimonials-section">
       <div className="absolute size-full flex flex-col items-center pt-[5vw]">
-        <h1 className="text-black first-title">What's</h1>
-        <h1 className="text-light-brown sec-title">Everyone</h1>
+        <h1 className="text-black first-title">weren't</h1>
+        <h1 className="text-light-brown sec-title">Alone!!!</h1>
       </div>
 
       <div className="pin-box">
